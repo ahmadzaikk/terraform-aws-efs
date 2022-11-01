bb2cea0562fe148f7c68372171557bfd6be8ec3a
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-11-01 11:30:48 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,16 @@
    description = "The file system ID"
  }
 
-output "arn" {
+output "file_system_arn" {
   value       = aws_efs_file_system.default.arn
   description = "EFS ARN"
 }
 output "access_points_arn" {
 value       = var.enabled ? { for arn in sort(keys(var.access_points)) : arn => aws_efs_access_point.default[arn].arn } : null
 description = "The access point list"
+}
+ 
+output "access_point_ids" {
+value       = var.enabled ? { for id in sort(keys(var.access_points)) : id => aws_efs_access_point.default[id].id } : null
+description = "The access point id"
 }
